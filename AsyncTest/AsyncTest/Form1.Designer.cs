9cfc0988cf7deedb7a15b345d2f0e8932eaae52c
jimgregor <jimmyg0801@gmail.com>
2016-12-07 15:03:29 -0500

    Added all text boxes and buttons

--- AsyncTest/AsyncTest/Form1.Designer.cs
+++ AsyncTest/AsyncTest/Form1.Designer.cs
@@ -28,21 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtSrcInput = new System.Windows.Forms.TextBox();
+            this.txtDstInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.ProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.lblPct = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnInpBrowse = new System.Windows.Forms.Button();
+            this.btnDstBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // txtSrcInput
+            // 
+            this.txtSrcInput.Location = new System.Drawing.Point(136, 3);
+            this.txtSrcInput.Name = "txtSrcInput";
+            this.txtSrcInput.Size = new System.Drawing.Size(382, 20);
+            this.txtSrcInput.TabIndex = 0;
+            this.txtSrcInput.Text = "L:\\Pictures\\Public\\Model Shoots\\Skips Studio\\Christina Banks";
+            // 
+            // txtDstInput
+            // 
+            this.txtDstInput.Location = new System.Drawing.Point(136, 29);
+            this.txtDstInput.Name = "txtDstInput";
+            this.txtDstInput.Size = new System.Drawing.Size(382, 20);
+            this.txtDstInput.TabIndex = 1;
+            this.txtDstInput.Text = "M:\\tmp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Destination Directory";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(136, 85);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus,
+            this.ProgressBar,
+            this.lblPct});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(608, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(39, 17);
+            this.lblStatus.Text = "Ready";
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(100, 16);
+            // 
+            // lblPct
+            // 
+            this.lblPct.Name = "lblPct";
+            this.lblPct.Size = new System.Drawing.Size(118, 17);
+            this.lblPct.Text = "toolStripStatusLabel2";
+            // 
+            // btnInpBrowse
+            // 
+            this.btnInpBrowse.Location = new System.Drawing.Point(524, 1);
+            this.btnInpBrowse.Name = "btnInpBrowse";
+            this.btnInpBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnInpBrowse.TabIndex = 6;
+            this.btnInpBrowse.Text = "Browse";
+            this.btnInpBrowse.UseVisualStyleBackColor = true;
+            this.btnInpBrowse.Click += new System.EventHandler(this.btnInpBrowse_Click);
+            // 
+            // btnDstBrowse
+            // 
+            this.btnDstBrowse.Location = new System.Drawing.Point(524, 27);
+            this.btnDstBrowse.Name = "btnDstBrowse";
+            this.btnDstBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnDstBrowse.TabIndex = 7;
+            this.btnDstBrowse.Text = "Browse";
+            this.btnDstBrowse.UseVisualStyleBackColor = true;
+            this.btnDstBrowse.Click += new System.EventHandler(this.btnDstBrowse_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(608, 261);
+            this.Controls.Add(this.btnDstBrowse);
+            this.Controls.Add(this.btnInpBrowse);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDstInput);
+            this.Controls.Add(this.txtSrcInput);
             this.Name = "Form1";
             this.Text = "Asynchronous File Copy";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtSrcInput;
+        private System.Windows.Forms.TextBox txtDstInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.ToolStripProgressBar ProgressBar;
+        private System.Windows.Forms.ToolStripStatusLabel lblPct;
+        private System.Windows.Forms.Button btnInpBrowse;
+        private System.Windows.Forms.Button btnDstBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- AsyncTest/AsyncTest/Form1.cs
+++ AsyncTest/AsyncTest/Form1.cs
@@ -12,6 +12,8 @@ namespace AsyncTest
 {
     public partial class Form1 : Form
     {
+        string srcPath, dstPath;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,30 @@ namespace AsyncTest
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            lblStatus.Text = "Ready";
+            ProgressBar.Visible = false;
+            lblPct.Visible = false;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInpBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtSrcInput.Text = srcPath = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnDstBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtSrcInput.Text = srcPath = folderBrowserDialog1.SelectedPath;
+            }
         }
     }
 }
