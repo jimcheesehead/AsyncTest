1ac74fd5d7324b88a4df7b373a8efb18d7bb2163
jimgregor <jimmyg0801@gmail.com>
2016-12-16 06:41:32 -0500

    Added better status text

--- AsyncTest/AsyncTest/Form1.cs
+++ AsyncTest/AsyncTest/Form1.cs
@@ -97,7 +97,13 @@ namespace AsyncTest
             info = DirOps.GetDirInfo(srcPath);
             totalFiles = info.totalFiles; /*************************************************************************/
 
-            lblStatus.Text = "Copying " + totalFiles.ToString() + " files in " + info.totalDirs + " directories";
+            string text = String.Format("Copying {0} files, {1} folders ({2})",
+                info.totalFiles, info.totalDirs, GetBytesReadable(info.totalBytes));
+            if (info.badLinks.Count() > 0)
+                text += String.Format(" - {0} bad links", info.badLinks.Count());
+            lblStatus.Text = text;
+
+            //lblStatus.Text = "Copying " + totalFiles.ToString() + " files in " + info.totalDirs + " directories";
             ProgressBar.Visible = true;
             lblPct.Visible = true;
 
@@ -265,6 +271,53 @@ namespace AsyncTest
 
             MessageBox.Show(errMsg);
             return true;
+        }
+
+        private string GetBytesReadable(long i)
+        {
+            // Get absolute value
+            long absolute_i = (i < 0 ? -i : i);
+            // Determine the suffix and readable value
+            string suffix;
+            double readable;
+            if (absolute_i >= 0x1000000000000000) // Exabyte
+            {
+                suffix = "EB";
+                readable = (i >> 50);
+            }
+            else if (absolute_i >= 0x4000000000000) // Petabyte
+            {
+                suffix = "PB";
+                readable = (i >> 40);
+            }
+            else if (absolute_i >= 0x10000000000) // Terabyte
+            {
+                suffix = "TB";
+                readable = (i >> 30);
+            }
+            else if (absolute_i >= 0x40000000) // Gigabyte
+            {
+                suffix = "GB";
+                readable = (i >> 20);
+            }
+            else if (absolute_i >= 0x100000) // Megabyte
+            {
+                suffix = "MB";
+                readable = (i >> 10);
+            }
+            else if (absolute_i >= 0x400) // Kilobyte
+            {
+                suffix = "KB";
+                readable = i;
+            }
+            else
+            {
+                return i.ToString("0 B"); // Byte
+            }
+            // Divide by 1024 to get fractional value
+            readable = (readable / 1024);
+            // Return formatted number with suffix
+            return readable.ToString("0.### ") + suffix;
         }
 
     }
